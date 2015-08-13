3647a4932ceea82473cccb6a391055f54a5a186d
Jason LaChapelle <jlachapelle@groupon.com>
2015-08-12 21:08:08 -0400

    Add action tests, rename cases to templates.

---
+++ test/kale.js
@@ -0,0 +1,79 @@
+var should = require('should');
+
+var kale = require('../index');
+var actions = require('../lib/actions');
+
+// -----
+//  Kale Tests
+// -----
+describe('kale tests', function() {
+  var pkg = require('../package.json');
+
+  it('should have the proper version', function() {
+    should.exist(kale.version);
+    kale.version.should.equal(pkg.version);
+  });
+
+  it('should have render function', function() {
+    should.exist(kale.render);
+    kale.render.should.be.a.Function();
+  });
+});
+
+// -----
+//  Action Tests
+// -----
+describe('action tests', function() {
+  it('should exist', function() {
+    actions.should.be.an.Object();
+  });
+
+  it('should have addAction function', function() {
+    should.exist(actions.addAction);
+    actions.addAction.should.be.a.Function();
+  });
+
+  it('should have hasAction function', function() {
+    should.exist(actions.hasAction);
+    actions.hasAction.should.be.a.Function();
+  });
+
+  // Standard Action Tests
+
+  // setupActionTest()
+  var setupActionTest = function setupActionTest(key, input, expected, subName) {
+    var name = key + '() should return expected result';
+    if ( subName != null ) name += ' [' + subName + ']';
+
+    it(name, function() {
+      var action = actions[key];
+      var result = actions[key].apply(null, input)
+
+      result.should.eql(expected);
+    });
+  }; //- setupActionTest()
+
+  var standardActions = require('./kale.json');
+  Object.keys(standardActions).forEach(function(key) {
+    it(key + ' should exist', function() {
+      actions.hasAction(key).should.be.True();
+      actions[key].should.be.a.Function();
+    });
+
+    var data = standardActions[key];
+    var input = data.input;
+    var expected = data.expected;
+
+    if ( Array.isArray(input) ) {
+      setupActionTest(key, input, expected);
+    }
+    else {
+      Object.keys(input).forEach(function(inputKey) {
+        var newInput = input[inputKey];
+        var newExpected = expected[inputKey];
+
+        setupActionTest(key, newInput, newExpected, inputKey);
+      });
+    }
+  });
+});
--- test/templates.js
+++ test/templates.js
@@ -5,7 +5,7 @@ var should = require('should');
 var kale = require('../index');
 
 // Get our test cases
-var cases = fs.readdirSync('test/cases')
+var templates = fs.readdirSync('test/templates')
   .filter(function(file) {
     return file.indexOf('kale') >= 0;
   })
@@ -18,23 +18,6 @@ var kaleOptions = {
   outPath: 'test/tmp',
   _banner: 'var $engine = require("../../lib/engine");'
 };
-
-// -----
-//  Kale Tests
-// -----
-describe('kale tests', function() {
-  var pkg = require('../package.json');
-
-  it('should have the proper version', function() {
-    should.exist(kale.version);
-    kale.version.should.equal(pkg.version);
-  });
-
-  it('should have render function', function() {
-    should.exist(kale.render);
-    kale.render.should.be.a.Function();
-  });
-});
 
 // -----
 //  Template Tests
@@ -51,12 +34,12 @@ describe('template tests', function() {
     }
   });
 
-  // Run our test cases
-  cases.forEach(function(test) {
+  // Run our test templates
+  templates.forEach(function(test) {
     var name = test.replace(/[-.]/g, ' ');
     it(name, function(done) {
-      var kalePath = __dirname + '/cases/' + test + '.kale';
-      var jsonPath = './cases/' + test + '.json';
+      var kalePath = __dirname + '/templates/' + test + '.kale';
+      var jsonPath = './templates/' + test + '.json';
 
       // Get our JSON and render our template
       var json = require(jsonPath);
